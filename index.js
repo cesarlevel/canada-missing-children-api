6f2350ef84f2a6bdbd3c4a15b76d0d3b77e09c07
Cesar Level <cesarlevel@gmail.com>
2023-08-14 15:09:14 -0400

    Update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const formatChars = require('./chars');
 const canadaMissingChildrenProvince = [
     'Alberta',
